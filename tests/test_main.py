19932aae76bd1668d212b7a1783e453e17fbf30f
DonPadla <vovavoroncov@yandex.ru>
2025-01-11 13:48:09 +0300

    create add_product, new_product, price

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,4 @@
-import pytest
-
 from main import Category, Product
-
-
-@pytest.fixture
-def product_first_phone():
-    """ Добавляем продукт """
-    return Product("phonei", "not iphone", 78900.0, 1)
 
 
 def test_init(product_first_phone):
@@ -17,29 +9,46 @@ def test_init(product_first_phone):
     assert product_first_phone.quantity == 1
 
 
-@pytest.fixture
-def category_phones():
-    """ Добавляем категорию """
-    return Category("phones", "devices for calls", ["youphone", "phonei"])
+def test_new_product(for_test_new_product):
+    assert for_test_new_product.name == "phonei over9000"
+    assert for_test_new_product.description == "not a joke"
+    assert for_test_new_product.price == 100500.0
+    assert for_test_new_product.quantity == 3
 
 
-def test_category(category_phones):
-    """ Тестируем инициализацию новой категории """
-    assert category_phones.name == "phones"
-    assert category_phones.description == "devices for calls"
-    assert category_phones.products == ["youphone", "phonei"]
+def test_price(product_first_phone):
+    product_first_phone.price = 100500.0
+    assert product_first_phone.price == 100500.0
 
 
-@pytest.fixture
-def category_calls():
-    """ Добавляем еще три категории """
-    category_1 = Category("a", "b", ["c", "d"])
-    category_2 = Category("1", "2", ["3", "4"])
-    category_3 = Category(".", ",", ["!", "?"])
-    return category_1, category_2, category_3
+def test_price_invalid(product_first_phone):
+    product_first_phone.price = 0.0
+    assert product_first_phone.price == 78900.0
+
+
+def test_category_init(category_calls):
+    assert category_calls.name == "phones"
+    assert category_calls.description == "phones for callsses"
 
 
 def test_counters(category_calls):
-    """ Тестируем счетчики """
-    assert Category.product_count == 8  # "youphone" и "phonei" тоже тут
-    assert Category.category_count == 4  # + "phones"
+    assert Category.product_count == 4
+    assert Category.category_count == 2
+
+
+def test_add_product_to_category():
+    product1 = Product("a", "b", 1.0, 1)
+    product2 = Product("c", "d", 2.0, 2)
+    product3 = Product("e", "f", 3.0, 3)
+    category = Category("abc", "def", [product1, product2, product3])
+    product4 = Product("g", "i", 4.0, 4)
+    category.add_product(product4)
+    assert Category.product_count == 8
+
+
+def test_products(one_category):
+    assert one_category.products == (
+        "a, b, 1.0 руб. Остаток: 1 шт.\n"
+        "c, d, 2.0 руб. Остаток: 2 шт.\n"
+        "e, f, 3.0 руб. Остаток: 3 шт.\n"
+    )
