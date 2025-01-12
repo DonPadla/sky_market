e89f24b7c8484a42353f5ff756c1358246cf210f
DonPadla <vovavoroncov@yandex.ru>
2025-01-12 12:05:49 +0300

    add isinstance(Product)

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
 
     def __init__(self, name, description, products):
         """ Функция инициализации категории """
@@ -63,8 +63,12 @@ class Category:
     def add_product(self, product: Product):
         """ Метод добавляющий новый продукт """
 
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+
+        else:
+            print("Не является объектом класса \"Product\"")
 
     @property
     def products(self):
@@ -78,50 +82,50 @@ class Category:
         return product_str
 
 
-# if __name__ == "__main__":
-#     product1 = Product(
-#     "Samsung Galaxy S23 Ultra",
-#     "256GB, Серый цвет, 200MP камера",
-#     180000.0,
-#     5)
-#     product2 = Product(
-#     "Iphone 15",
-#     "512GB, Gray space",
-#     210000.0,
-#     8)
-#     product3 = Product("
-#     Xiaomi Redmi Note 11",
-#     "1024GB, Синий",
-#     31000.0,
-#     14)
-#
-#     category1 = Category(
-#         "Смартфоны",
-#         "Смартфоны, как средство не только коммуникации,
-#         но и получения дополнительных функций для удобства жизни",
-#         [product1, product2, product3]
-#     )
-#
-#     print(category1.products)
-#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-#     category1.add_product(product4)
-#     print(category1.products)
-#     print(category1.product_count)
-#
-#     new_product = Product.new_product(
-#         {"name": "Samsung Galaxy S23 Ultra",
-#         "description": "256GB, Серый цвет, 200MP камера",
-#         "price": 180000.0,
-#          "quantity": 5})
-#     print(new_product.name)
-#     print(new_product.description)
-#     print(new_product.price)
-#     print(new_product.quantity)
-#
-#     new_product.price = 800
-#     print(new_product.price)
-#
-#     new_product.price = -100
-#     print(new_product.price)
-#     new_product.price = 0
-#     print(new_product.price)
+if __name__ == "__main__":
+    product1 = Product(
+    "Samsung Galaxy S23 Ultra",
+    "256GB, Серый цвет, 200MP камера",
+    180000.0,
+    5)
+    product2 = Product(
+    "Iphone 15",
+    "512GB, Gray space",
+    210000.0,
+    8)
+    product3 = Product(
+        "Xiaomi Redmi Note 11",
+    "1024GB, Синий",
+    31000.0,
+    14)
+
+    category1 = Category(
+        "Смартфоны",
+        """Смартфоны, как средство не только коммуникации,
+        но и получения дополнительных функций для удобства жизни""",
+        [product1, product2, product3]
+    )
+
+    print(category1.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
